f99477cff82091ecbc72fe0464c8018e45866eba
mdmrk <mariodavo.20@gmail.com>
2025-06-11 15:55:47 +0200

    route names

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,15 +7,43 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: "Preguntas"
+      }
     },
     {
       path: "/quiz/:quizId",
       name: "quiz",
       component: () => import("@/views/QuizView.vue"),
-      props: true
+      props: true,
+      meta: {
+        title: "Preguntas"
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const baseTitle = "Preguntas"
+
+  if (to.name === "quiz") {
+    const quizId = to.params.quizId as string
+    const formattedTitle = quizId
+      .split(/[-_]/)
+      .map((word, index) => {
+        if (index === 0) {
+          return word.toUpperCase()
+        }
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      })
+      .join(" ")
+    document.title = `${baseTitle} - ${formattedTitle}`
+  } else {
+    document.title = baseTitle
+  }
+
+  next()
 })
 
 export default router
