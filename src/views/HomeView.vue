30e43df8824008dd3b99b3df00f3182b0e8ca90a
mdmrk <mariodavo.20@gmail.com>
2025-06-17 23:29:08 +0200

    renaming

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,9 +18,9 @@ const ppss = ref(false)
         >
           <MainButton id="ada-parcial-1" class="gradient-red">parcial 1</MainButton>
           <MainButton id="ada-parcial-2" class="gradient-red">parcial 2</MainButton>
-          <MainButton id="ada-completo" class="gradient-red">completo</MainButton>
-          <MainButton id="ada-2024" class="gradient-red">2024</MainButton>
-          <MainButton id="ada-2025" class="gradient-red">2025</MainButton>
+          <MainButton id="ada-final" class="gradient-red">final</MainButton>
+          <MainButton id="ada-final-24" class="gradient-red">final '24</MainButton>
+          <MainButton id="ada-final-25" class="gradient-red">final '25</MainButton>
         </div>
       </div>
       <div class="relative w-full flex flex-col items-center">
