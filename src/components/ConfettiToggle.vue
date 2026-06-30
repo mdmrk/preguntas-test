452f29317b45982a983dd622372dd8713f4a0e00
mdmrk <mariodavo.20@gmail.com>
2026-06-30 14:02:50 +0200

    show confetti off iccon

--- src/components/ConfettiToggle.vue
+++ src/components/ConfettiToggle.vue
@@ -5,12 +5,14 @@
     :aria-label="confettiEnabled ? 'Disable confetti' : 'Enable confetti'"
     :aria-pressed="confettiEnabled"
   >
-    <ConfettiIcon class="w-5 h-5" :class="{ 'opacity-40': !confettiEnabled }" />
+    <ConfettiIcon v-if="confettiEnabled" class="w-5 h-5" />
+    <ConfettiOffIcon v-else class="w-5 h-5 opacity-40" />
   </button>
 </template>
 
 <script setup lang="ts">
 import ConfettiIcon from "@/components/icons/ConfettiIcon.vue"
+import ConfettiOffIcon from "@/components/icons/ConfettiOffIcon.vue"
 import { useConfettiEnabled } from "@/composables/useConfettiEnabled"
 
 const { confettiEnabled, toggleConfetti } = useConfettiEnabled()
